0ca1560558f593fb1ea272526c7fe24fb4d65688
bibinmekkattukunnil-biju <bmekkattukunnil-biju@myseneca.ca>
2022-06-05 23:10:02 +0000

    Tf files

---
+++ terraform/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Bibin"
+    "App"   = "Static Web"
+    "Project" = "CLO835Assignmnt"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "AG-1-catsndogs"
+}
